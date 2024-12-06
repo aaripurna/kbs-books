18ee56059e8c113c2cda82b9c806da0419560348
Nawa Aripurna <nap.aripurna@gmail.com>
2024-12-06 16:40:25 +0800

    add author create

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use App\Http\Requests\AuthorCreateRequest;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Redirect;
 
 class AuthorController extends Controller
 {
@@ -11,5 +14,20 @@ class AuthorController extends Controller
     {
         $users = \App\Models\Author::query()->paginate(32);
         return view("author.index", ['users' => $users]);
+    }
+
+    public function create(Request $request): View
+    {
+        $author = new \App\Models\Author();
+        return view("author.create", ['author' => $author]);
+    }
+
+    public function store(AuthorCreateRequest $request): RedirectResponse
+    {
+        $validated = $request->validated();
+
+        \App\Models\Author::create($validated);
+
+        return Redirect::route('authors.index');
     }
 }
